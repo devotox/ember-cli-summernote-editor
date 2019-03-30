a8c0b0ce45c4a9fac326aa8cbbd49f4191d9cce1
Devonte <devo.tox.89@gmail.com>
2019-03-30 09:45:18 +0000

    Update packages

--- addon/components/summernote-editor.js
+++ addon/components/summernote-editor.js
@@ -35,6 +35,7 @@ export default Component.extend({
   placeholder: null,
   dialogsInBody: false,
   disableDragAndDrop: false,
+  blockquoteBreakingLevel: 2,
 
   summernoteOptions() {
     let buttons = {};
@@ -45,7 +46,7 @@ export default Component.extend({
 
     let properties = this.getProperties([
       'height', 'focus', 'lang', 'airMode', 'shortcuts', 'placeholder',
-      'dialogsInBody', 'dialogsFade', 'disableDragAndDrop'
+      'dialogsInBody', 'dialogsFade', 'disableDragAndDrop', 'blockquoteBreakingLevel'
     ]);
 
     _buttons.forEach((item, i) => {
